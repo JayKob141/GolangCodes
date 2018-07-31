ae0c8fe3108eabb6cb4cf776d2654ba5bb66ba71
Jacob Meneses <jaykobyou@gmail.com>
2018-07-30 21:53:45 -0500

    jobshop: Init population of genetic algorithm

--- jobshop-genetic-algorithm/jobshop.go
+++ jobshop-genetic-algorithm/jobshop.go
@@ -6,6 +6,9 @@ import (
     "bufio"
     "strings"
     "strconv"
+    "math/rand"
+    "time"
+	"sync"
 )
 
 type JobShopSpecification struct {
@@ -15,14 +18,12 @@ type JobShopSpecification struct {
     timesMatrix [][]int
 }
 
-/*
-struct GeneticAlgorithmParameters {
+type GeneticAlgorithmParameters struct {
     numberOfGenerations int
     populationSize int
     percentOfCross float64
     percentOfMutation float64
 }
-*/
 
 func readFile(fullPath string) JobShopSpecification {
 
@@ -97,26 +98,94 @@ func main() {
     argsWithoutProg := os.Args[1:]
 
     if len(argsWithoutProg) == 0 {
-        fmt.Println("Expected 2 argument (instance file and parameters file)")
+        fmt.Println("Expected 1 argument (instance file)")
         return
     }
     
     fmt.Println(argsWithoutProg)
 
-    readFile( argsWithoutProg[0] )
-     
-    // TODO: parse configuration and GA parameters file
+    jobshop := readFile( argsWithoutProg[0] )
 
-    geneticAlgorithm()
+    ga := GeneticAlgorithmParameters { numberOfGenerations: 16, populationSize: 16, percentOfCross: 0.5, percentOfMutation: 0.1 }
+     
+    geneticAlgorithm(ga, jobshop)
 }
 
-func geneticAlgorithm(){
-    //  TODO: Init population
 
-    // TODO: repeat N generations
-        // selection of individuals 
+func fillWithRandomJobs(sequences []int, sequenceSize, numberOfSequences, numberOfJobs, numberOfMachines, idGoRoutine int, wg *sync.WaitGroup) {
+    defer wg.Done()
+
+    counters := make([]int, numberOfJobs)
+    r := rand.New( rand.NewSource( time.Now().Unix() * int64(idGoRoutine) * 17) )
+
+    for s := 0 ; s < numberOfSequences ; s++ {
+
+        low := s * sequenceSize
+        high := low + sequenceSize
+        sequence := sequences[ low:high ]
+        for j := 0 ; j < numberOfJobs; j++ {
+            counters[j] = 0
+        }
+
+        for i := 0 ; i < sequenceSize; i++ {
+            randomJob := r.Intn( numberOfJobs )
+            for counters[randomJob] >= numberOfMachines {
+                randomJob = r.Intn( numberOfJobs )
+            }
+            sequence[i] = randomJob
+            counters[randomJob] += 1
+        }
+    }
+}
+
+func geneticAlgorithm(ga GeneticAlgorithmParameters, jobshop JobShopSpecification){
+
+    numberOfGoRoutines := 4
+    
+    //  Init population
+    sequenceSize := jobshop.numberOfJobs * jobshop.numberOfMachines
+    sequences := make( []int, ga.populationSize * sequenceSize )
+    // makespans := make( []int, ga.populationSize )
+    // ganttChart := make( []int, ga.populationSize * jobshop.numberOfMachines )
+    // previousFinalTimes :=  make( []int, ga.populationSize * jobshop.numberOfJobs )
+    // iOperation :=  make( []int, ga.populationSize * jobshop.numberOfJobs )
+    // indices := make( []int, ga.populationSize )
+
+    // generate random init population
+	var wg sync.WaitGroup
+    wg.Add(numberOfGoRoutines)
+    N := ga.populationSize / numberOfGoRoutines
+    for r := 0 ; r < numberOfGoRoutines ; r++{
+        low := r * N * sequenceSize
+        high := low + N * sequenceSize
+        go fillWithRandomJobs( sequences[low:high], sequenceSize,N, jobshop.numberOfJobs, jobshop.numberOfMachines, r , &wg)
+    }
+    wg.Wait()
+
+    for s := 0 ; s < ga.populationSize ; s++ {
+        low := s * sequenceSize
+        high := low + sequenceSize
+        printableSequence := sequences[low:high]
+        for _, value := range printableSequence {
+            fmt.Printf("%d ", value)
+        }
+        fmt.Println()
+    }
+
+    // TODO: compute makespans
+
+    for g := 0 ; g < ga.numberOfGenerations ; g++ {
+        // TODO:
+        // sort sequences by makespan
+
+        // selection of individuals
+        
         // cross operation
         // mutation operation
+
+        // evaluate population (compute makespans)
+
         // update solution with minimal cost
+    }
     
 }

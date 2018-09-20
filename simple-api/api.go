85d902f4fa4f1bc56c205c3557f4eecd810a9ef6
Jacob Meneses <jaykobyou@gmail.com>
2018-09-20 16:42:53 -0500

    api: example of insert of sql module

--- simple-api/api.go
+++ simple-api/api.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"encoding/json"
-	"net/http"
-	"database/sql"
-	_ "github.com/lib/pq"
+  "fmt"
+  "log"
+  "encoding/json"
+  "strconv"
+  "net/http"
+  "database/sql"
+  _ "github.com/lib/pq"
 )
 
 type User struct {
@@ -16,17 +17,49 @@ type User struct {
 	LeaveDate string `json:"leaveDate"`
 }
 
-func readUsers() []User {
+type ApiDb struct {
+  db *sql.DB
+}
+
+func crearApiDbObject () ApiDb {
  connStr := "user=postgres password='example' dbname='postgres' host=localhost port=5432 sslmode=disable"
+  db, err := sql.Open("postgres", connStr)
+  apiDb := ApiDb{ db: db }
 
- db, err := sql.Open("postgres", connStr)
+  if err != nil {
+     fmt.Println(err.Error())
+  } else {
+     fmt.Println("Connected to database")
+  }
 
+  return apiDb
+}
+
+func writeUser(newUser User) User{
+ apiDb := crearApiDbObject()
+ db := apiDb.db
+ defer db.Close()
+
+ result, err := db.Exec("INSERT INTO users (name) values ($1)", newUser.Name )
  if err != nil {
-	 fmt.Println(err.Error())
+	fmt.Println("Error executing query")
+	fmt.Println(err.Error())
  } else {
-	 defer db.Close()
-	 fmt.Println("Connected to database")
+   id, err2 := result.LastInsertId()
+   if err2 != nil {
+     fmt.Println("Cannot obtain LastInsertId")
+     fmt.Println(err2.Error())
+   }
+   newUser.Id = strconv.FormatInt( id, 10)
  }
+
+ return newUser
+}
+
+func readUsers() []User {
+ apiDb := crearApiDbObject()
+ db := apiDb.db
+ defer db.Close()
 
  users := make([]User, 0)
 
@@ -62,9 +95,19 @@ func listEntries(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
 }
+
 func registerEnter(w http.ResponseWriter, r *http.Request) {
+  newUser := User{ Name: "From golang" }
+  newUser = writeUser(newUser)
+
+	b, _ := json.Marshal(newUser)
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
 
 }
+
 func registerLeave(w http.ResponseWriter, r *http.Request) {
 	// user := User{ Id: "123", Name: "Jacob Meneses", EnterDate: "2018-01-01", LeaveDate: "2018-02-02" }
 
@@ -80,6 +123,6 @@ func main() {
 	http.HandleFunc("/register-enter/", registerEnter)
 	http.HandleFunc("/register-leave/", registerLeave)
 
-	fmt.Println("Running server on: http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Running server on: http://localhost:8081")
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
